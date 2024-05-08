299946fb679203595940188629a716f116a7b01e
abhay2003vyas <142084995+abhay2003vyas@users.noreply.github.com>
2024-05-08 16:33:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const navLinks = document.querySelector('.nav-links');
 toggleButton.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+            const toggleButton = document.querySelector('.toggle-button');
+            const navLinks = document.querySelector('.nav-links');
+
+            toggleButton.addEventListener('click', function () {
+                navLinks.classList.toggle('show');
+            });
+        });
+
 let slideIndex = 0;
 showSlides();
 
